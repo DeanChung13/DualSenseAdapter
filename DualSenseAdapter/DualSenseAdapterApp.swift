b978d75ab61a08c8b6b834f1ea88b68ebdd7fee3
Dean Chung <wenway13@gmail.com>
2025-09-24 15:47:54 +0800

    Initial Commit

---
+++ DualSenseAdapter/DualSenseAdapterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualSenseAdapterApp.swift
+//  DualSenseAdapter
+//
+//  Created by Dean Chung on 2025/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DualSenseAdapterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
